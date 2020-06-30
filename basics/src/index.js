fc458c1702f12f645bb0df328cffcfd4f46d1ae8
mohamedisakr <mohamedisakr@gmail.com>
2020-06-30 13:43:12 +0200

    Working with Arrays Part II

--- basics/src/index.js
+++ basics/src/index.js
@@ -1,10 +1,49 @@
 import { GraphQLServer } from "graphql-yoga";
 
+const users = [
+  {
+    id: "7dbf629f-7ab3-467a-a9b6-6caccbc6c72e",
+    name: "Andrew",
+    email: "andrew@example.com",
+  },
+  {
+    id: "a68c2a52-41ad-4595-b412-48006378cb91",
+    name: "Sarah",
+    email: "sarah@example.com",
+  },
+  {
+    id: "061ef0d3-f13f-4c68-badf-e1379e3feef7",
+    name: "Michael",
+    email: "michael@example.com",
+  },
+];
+
+const posts = [
+  {
+    id: "83c00037-edf0-4676-8ce6-2b890175519b",
+    title: "GraphQL 101",
+    body: "This is how to use GraphQL...",
+    published: true,
+  },
+  {
+    id: "8947b535-c36c-4cc3-8851-64283065b995",
+    title: "GraphQL 201",
+    body: "This is an advanced GraphQL post...",
+    published: false,
+  },
+  {
+    id: "5998f660-06f6-4eeb-a5b3-cf637105cf6a",
+    title: "Programming Music",
+    body:
+      "David Cutter Music is my favorite artist to listen to while programming.",
+    published: false,
+  },
+];
 // type definitions  >> schema
 const typeDefs = `
   type Query {
-      add(numbers : [Float!]!) : Float!
-      greeting(name: String): String!
+      users(query: String) : [User!]!
+      posts(query: String) : [Post!]!
       me : User!  
       post : Post!    
   }
@@ -27,13 +66,24 @@ const typeDefs = `
 // resolverers
 const resolvers = {
   Query: {
-    add: (_, { numbers }) => {
-      if (numbers.length === 0) {
-        return 0;
+    users: (_, { query }) => {
+      if (!query) {
+        return users;
       }
-      return numbers.reduce((acc, cur) => acc + cur);
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(query.toLowerCase())
+      );
     },
-    greeting: (_, { name }) => `Hello ${name || "World"}`,
+    posts: (_, { query }) => {
+      if (!query) {
+        return posts;
+      }
+      return posts.filter(
+        (post) =>
+          post.title.toLowerCase().includes(query.toLowerCase()) ||
+          post.body.toLowerCase().includes(query.toLowerCase())
+      );
+    },
     me() {
       return {
         id: "1234789",
